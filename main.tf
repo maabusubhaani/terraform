59db850ab2ae39f25847bdfd837d397d57b8c81e
maabusubhaani <subhaanis61@gmail.com>
2025-03-07 12:21:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region ="us-east-1"
 }
 resource "aws_instance" "two" {      
-  count         =2
+  count         =10
   ami           = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   tags = {
